ad3e9fab6a3c34851231b1a7e5044e59eee7bec2
Tony <thunter1987@yahoo.com>
2023-04-20 00:06:14 -0500

    "Tests Passed"

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const elemMain = document.getElementById("main");
+elemMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'Tony is the champion';
+document.head.append(newHeader);
